08dc368271758c3e3de1ccda1a314c3426a8dcae
ingridsalazar <ingridalejandrasalazar@gmail.com>
2017-10-02 15:45:13 -0500

    Update GithubApi.Authentication.test.js

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -3,14 +3,14 @@ const statusCode = require('http-status-codes');
 const { expect } = require('chai');
 
 const urlBase = 'https://api.github.com';
-const githubUserName = 'AgileTestingColombia';
-const repository = 'workshop-api-testing-js';
+const githubUserName = 'ingridsalazar';
+const repository = 'psl-workshop-api-testing-js';
 export ACCESS_TOKEN = 'f08bd589637b40829ab7687fb59c70e6102e36f3';
 
 describe('Github Api Test', () => {
  describe('Authentication', () => {
    it('Via OAuth2 Tokens by Header', () =>
-     agent.get(`${urlBase}/repos/${ingridsalazar}/${psl-workshop-api-testing-js}`)
+     agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
        .auth('token', process.env.ACCESS_TOKEN)
        .then((response) => {
          expect(response.status).to.equal(statusCode.OK);

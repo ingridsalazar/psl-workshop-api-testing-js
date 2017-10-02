98afebeb2910c1f84565e375a9e24f2959bc1be3
PSLCOL\isalazare <isalazar@psl.com.co>
2017-10-02 16:41:24 -0500

    Authorization

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -5,16 +5,23 @@ const { expect } = require('chai');
 const urlBase = 'https://api.github.com';
 const githubUserName = 'ingridsalazar';
 const repository = 'psl-workshop-api-testing-js';
-export ACCESS_TOKEN = 'f08bd589637b40829ab7687fb59c70e6102e36f3';
 
 describe('Github Api Test', () => {
- describe('Authentication', () => {
-   it('Via OAuth2 Tokens by Header', () =>
-     agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-       .auth('token', process.env.ACCESS_TOKEN)
-       .then((response) => {
-         expect(response.status).to.equal(statusCode.OK);
-         expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
-       }));
- });
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', () =>
+      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+        }));
+
+    it('Via OAuth2 Tokens by parameter', () =>
+      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+        .query(`access_token=${process.env.ACCESS_TOKEN}`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+        }));
+  });
 });
